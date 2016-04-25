5e45c406fabf12662592bae95f164ddfc8a3db4c
Andy <rym1112ov@gmail.com>
2016-04-25 03:24:25 +0300

    make responsive

--- js/javascript.js
+++ js/javascript.js
@@ -1,19 +1,41 @@
-var images = ['../evestudio/images/eagle.jpg','../evestudio/images/photo.jpg','../evestudio/images/phone.jpg','../evestudio/images/comp.jpg'];
-function showPage( iD ){
+
+$(document).ready(function(){
+	$('.wrap').find('> .sect:eq(0)').nextAll('.sect').css({'opacity':'0','display':'none'});//hide all but first
+
+	var currentslide = $('.sect:eq(0)');
+	var currentposition = $('.wrap .sect').index(currentslide);
+
+	// On click of a controller link...
+	$('.slider_item').click(function(e){
+		e.preventDefault();
+		$('.slider_line').css("opacity",0.5);
+	    $('.slider_line .slider_line-mod').css("opacity",0);
+	    var abc = $(this).attr('data-href');
+		
+		
+		
+		
+		// Fade the current slide out...
+		$('.wrap .sect:eq('+currentposition+')').animate({opacity: 0}, 250, function() {
+			
+			// ... and hide it.
+			$('.wrap .sect:eq('+currentposition+')').css('display','none');
+
+			// Show the next slide...
+			$('.wrap .sect:eq('+ abc+')').css('display','block');
+
+			// ... and fade it in.
+			$('.wrap .sect:eq('+ abc+')').animate({opacity: 1}, 800);
+			
+		  }
+		);
+	  currentposition =  abc;
 	  
-      var pID = images[iD];
-
-      $('.sect').css({"background":"url("+pID+") no-repeat","background-size":"cover","background-position":"center"});
-      
-      
-    }
-showPage(0);
-
-$('.slider_item').click(function(){
-  $('.slider_line').css("opacity",0.5);
-  $('.slider_line .slider_line-mod').css("opacity",0);
-  showPage( +$(this).attr('data-id') );
-  $(this).children('.slider_line').css("opacity",1);
-  $(this).find('.slider_line-mod').css("opacity",1);
+	  $(this).children('.slider_line').css("opacity",1);
+	  $(this).find('.slider_line-mod').css("opacity",1);
+	});
+	$( ".slider_item" ).hover(function() {
+	  $(this).children('.slider_line').css("opacity",1);
+	  $(this).find('.slider_line-mod').css("opacity",1);
+	});
 });
-
